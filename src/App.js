afa35393a5adc558d5308479a197282b2abdd8bd
Warakorn <boydoubleb@gmail.com>
2021-01-29 17:42:47 +0700

    init: project :rocket:

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className='container'>
+      <div className='login_register-container'>
+        <div className='login_register-card'>
+          <h1>Welcome</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
